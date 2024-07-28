40dbac61e8667297acda6166b0bb79e641f0425b
khushi667 <khushi.mis03@gmail.com>
2024-07-28 09:47:42 +0530

    demo navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
     <>
-      <p className="border border-red-500">This is StockMarket</p>
+      <Navbar/>
     </>
   )
 }
