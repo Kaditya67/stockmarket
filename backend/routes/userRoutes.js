724142c3dd855c2461e700c907b12bf0333c1f2d
khushi667 <khushi.mis03@gmail.com>
2024-11-24 16:03:09 +0530

    update on 24/11/24

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,10 @@ import { postSignup, getSignup, login } from '../controllers/userController.js';
 
 const userRoutes = express.Router();
 
-// POST route for signup (to create a new user)
 userRoutes.post('/signup', postSignup);
 
-// Optional GET route for signup (can be used for serving forms or basic information)
 userRoutes.get('/signup', getSignup);
 
-// POST route for login
 userRoutes.post('/login', login);
 
 export default userRoutes;
